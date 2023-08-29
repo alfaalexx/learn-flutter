ad3e422a6a869b2233064209b2136adb7e192d24
alfaalexx <alfaalex02@gmail.com>
2023-08-29 08:13:49 +0700

    mengubah text menjadi lebih dinamis

--- lib/styled_text.dart
+++ lib/styled_text.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 
 class StyledText extends StatelessWidget {
-  const StyledText({super.key});
+  const StyledText(this.outputText, {super.key});
+
+  final String outputText;
 
   @override
   Widget build(context) {
-    return const Text(
-      'Hello, My Name is Alfa Alexandra Simanjuntak. Nice to meet you!',
-      style: TextStyle(
+    return Text(
+      outputText,
+      style: const TextStyle(
         color: Colors.white,
         fontSize: 20,
       ),
